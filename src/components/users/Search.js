fd65c5bff4a21b0e5c880129585f1a84147a7e3c
Nazarov Petr <mrzueck@gmail.com>
2019-07-06 20:20:19 +1000

    introducing u state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,57 +1,53 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
-export class Search extends Component {
-  state = {
-    text: ""
-  };
-  onSubmit = event => {
+const Search = props => {
+  const [text, setText] = useState('');
+  const onSubmit = event => {
     event.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("Please enter something", "danger");
+    if (text === "") {
+      props.setAlert("Please enter something", "danger");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      props.searchUsers(text);
+      setText('');
     }
   };
-  onClear = event => {
+  const onClear = event => {
     event.preventDefault();
-    this.props.clearUsers();
+    props.clearUsers();
   };
-  onChange = event =>
-    this.setState({ [event.target.name]: event.target.value });
-  render() {
-    return (
-      <div className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column  is-10 field">
-              <input
-                className="input"
-                name="text"
-                type="text"
-                placeholder="Search users"
-                value={this.state.text}
-                onChange={this.onChange}
-              />
-            </div>
+  const onChange = event => setText(event.target.value );
+
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column  is-10 field">
+            <input
+              className="input"
+              name="text"
+              type="text"
+              placeholder="Search users"
+              value={text}
+              onChange={onChange}
+            />
+          </div>
+          <div className="column is-1">
+            <button onClick={onSubmit} className="button is-primary">
+              Search
+            </button>
+          </div>
+          {props.showClear && (
             <div className="column is-1">
-              <button onClick={this.onSubmit} className="button is-primary">
-                Search
+              <button onClick={onClear} className="button is-secondary">
+                Clear
               </button>
             </div>
-            {this.props.showClear && (
-              <div className="column is-1">
-                <button onClick={this.onClear} className="button is-secondary">
-                  Clear
-                </button>
-              </div>
-            )}
-          </div>
+          )}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 Search.propTypes = {
   searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
